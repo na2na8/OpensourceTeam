4ff0d83eed0ccd20a5268728d3ba003e1a9722e6
hoonski <30435278+hoonski@users.noreply.github.com>
2017-10-19 17:07:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kwon_younghoon/opensourceteam/OpenSourceTeam.java
@@ -0,0 +1,13 @@
+package com.example.kwon_younghoon.opensourceteam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenSourceTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_source_team);
+    }
+}
